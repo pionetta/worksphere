b3fcf82e0aed71cf3f46d45978d9d7d2598ac332
Rusdi Aristiawan <pionettaaltabell@gmail.com>
2026-08-30 18:09:02 +0800

    chore: update service worker build

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b696421e'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7dbkp0jof3c"
+    "revision": "0.eeo9aet738c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
